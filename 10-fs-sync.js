b02c5819883b52db70b6a19e2402dfa7c50a733a
0-Serotonin <tomthein2@gmail.com>
2022-03-13 18:10:19 +0800

    first commit

---
+++ 10-fs-sync.js
@@ -0,0 +1,9 @@
+const {readFileSync, writeFileSync, writeFile} = require('fs')
+
+const firstFile = readFileSync('./content/first.txt' , 'utf-8')
+
+const secondFile = readFileSync('./content/second.txt', 'utf-8')
+
+console.log(firstFile,secondFile);
+
+writeFileSync('./content/result-sync.txt', `Here is the result ${firstFile}, ${secondFile}`, {flag: 'a'})
---
+++ 6-alternative.js
@@ -0,0 +1,8 @@
+const item = ['itesm1', 'item2']
+
+const person = {
+    name: "John",
+    age: 23
+}
+
+module.exports = {item, person}
